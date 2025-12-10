59f0cbdeaae48bc8d37df6282c9f4b9b62bef6e7
garcia-bj <garciacussi7@gmail.com>
2025-12-10 14:43:52 -0400

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,9 @@ async function bootstrap() {
 
   // Enable CORS for frontend
   app.enableCors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://chipper-taiyaki-c6c911.netlify.app"],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
